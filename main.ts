f20f61a26c0e7686d9ecd49ed5448d9fad539cc9
takejohn <takejohn@takejohn.jp>
2025-01-19 14:12:40 +0900

    Formatting

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
-import { serveDir } from "@std/http";
+import { serveDir } from '@std/http';
 
-const userPagePattern = new URLPattern({ pathname: "/users/:id" });
-const staticPathPattern = new URLPattern({ pathname: "/static/*" });
+const userPagePattern = new URLPattern({ pathname: '/users/:id' });
+const staticPathPattern = new URLPattern({ pathname: '/static/*' });
 
 export default {
-  fetch(req) {
-    const url = new URL(req.url);
+    fetch(req) {
+        const url = new URL(req.url);
 
-    if (url.pathname === "/") {
-      return new Response("Home page");
-    }
+        if (url.pathname === '/') {
+            return new Response('Home page');
+        }
 
-    const userPageMatch = userPagePattern.exec(url);
-    if (userPageMatch) {
-      return new Response(userPageMatch.pathname.groups.id);
-    }
+        const userPageMatch = userPagePattern.exec(url);
+        if (userPageMatch) {
+            return new Response(userPageMatch.pathname.groups.id);
+        }
 
-    if (staticPathPattern.test(url)) {
-      return serveDir(req);
-    }
+        if (staticPathPattern.test(url)) {
+            return serveDir(req);
+        }
 
-    return new Response("Not found", { status: 404 });
-  },
+        return new Response('Not found', { status: 404 });
+    },
 } satisfies Deno.ServeDefaultExport;
